3d0a72b9f8ad76129b66589bac60b1da2cd911e6
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2021-01-23 20:07:51 -0300

    Adicionado Models

---
+++ AtividadeFinal/CadastroAluno/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroAluno.Models
+{
+    class Aluno
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<Endereco> Enderecos { get; set; }
+    }
+}
---
+++ AtividadeFinal/CadastroAluno/Models/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroAluno.Models
+{
+    class Endereco
+    {
+        public string Tipo { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+
+    }
+}
